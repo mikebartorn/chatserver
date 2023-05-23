54a7bf5b5858e8cb148c5839020be49bcae76f80
xzb <1318270748@qq.com>
2023-05-24 04:12:58 +0800

    main.version

---
+++ build/src/client/CMakeFiles/ChatClient.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/abin/code/chatserver/chatserver/src/client/main.cpp" "/home/abin/code/chatserver/chatserver/build/src/client/CMakeFiles/ChatClient.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../include/server"
+  "../include/server/database"
+  "../include/server/model"
+  "../include/server/redis"
+  "../thirdparty"
+  "/usr/include/mysql"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/src/server/CMakeFiles/ChatServer.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/abin/code/chatserver/chatserver/src/server/chatserver.cpp" "/home/abin/code/chatserver/chatserver/build/src/server/CMakeFiles/ChatServer.dir/chatserver.cpp.o"
+  "/home/abin/code/chatserver/chatserver/src/server/chatservice.cpp" "/home/abin/code/chatserver/chatserver/build/src/server/CMakeFiles/ChatServer.dir/chatservice.cpp.o"
+  "/home/abin/code/chatserver/chatserver/src/server/database/database.cpp" "/home/abin/code/chatserver/chatserver/build/src/server/CMakeFiles/ChatServer.dir/database/database.cpp.o"
+  "/home/abin/code/chatserver/chatserver/src/server/main.cpp" "/home/abin/code/chatserver/chatserver/build/src/server/CMakeFiles/ChatServer.dir/main.cpp.o"
+  "/home/abin/code/chatserver/chatserver/src/server/model/friendmodel.cpp" "/home/abin/code/chatserver/chatserver/build/src/server/CMakeFiles/ChatServer.dir/model/friendmodel.cpp.o"
+  "/home/abin/code/chatserver/chatserver/src/server/model/groupmodel.cpp" "/home/abin/code/chatserver/chatserver/build/src/server/CMakeFiles/ChatServer.dir/model/groupmodel.cpp.o"
+  "/home/abin/code/chatserver/chatserver/src/server/model/offlinemessagemodel.cpp" "/home/abin/code/chatserver/chatserver/build/src/server/CMakeFiles/ChatServer.dir/model/offlinemessagemodel.cpp.o"
+  "/home/abin/code/chatserver/chatserver/src/server/model/usermodel.cpp" "/home/abin/code/chatserver/chatserver/build/src/server/CMakeFiles/ChatServer.dir/model/usermodel.cpp.o"
+  "/home/abin/code/chatserver/chatserver/src/server/redis/redis.cpp" "/home/abin/code/chatserver/chatserver/build/src/server/CMakeFiles/ChatServer.dir/redis/redis.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "../include/server"
+  "../include/server/database"
+  "../include/server/model"
+  "../include/server/redis"
+  "../thirdparty"
+  "/usr/include/mysql"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
